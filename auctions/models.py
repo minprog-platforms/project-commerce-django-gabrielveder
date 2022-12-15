01dcbb454bb5e5bb4c7859726eb2b4952d1f9f4e
Gabriel Veder <gabriel.veder@gmail.com>
2022-12-15 13:03:07 +0100

    Added a functioning index which displays all active listings

--- auctions/models.py
+++ auctions/models.py
@@ -6,11 +6,16 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
+
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=1000)
     current_bid = models.PositiveIntegerField()
     image_url = models.CharField(max_length=64)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listing_user")
+    isactive = models.BooleanField(default=True)
+
+    def __repr__(self) -> str:
+        return self.title
 
 class Bid(models.Model):
     amount = models.PositiveIntegerField()
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,18 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing, Bid
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.filter(isactive=True)
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 
 def login_view(request):
@@ -62,8 +66,31 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+@login_required
 def new_listing(request):
+    if request.method == "POST":
+        url = request.POST["url"]
+        listing_title = request.POST["title"]
+        description = request.POST["description"]
+        current_bid = request.POST["start_price"]
+        new_listing = Listing(
+            title = listing_title,
+            description = description,
+            current_bid = current_bid,
+            owner = request.user,
+            image_url = url
+            )
+        new_listing.save()
+        return render(request, "auctions/index.html")
+    else:
+        return render(request, "auctions/new_listing.html")
+
+
+def listing_page(request, id):
     if request.method == "POST":
         pass
     else:
-        return render(request, "auctions/new_listing.html")
+        listing = Listing.objects.get(pk=id)
+        return render(request, "auctions/listing_page.html",{
+            "listing":listing
+        })
